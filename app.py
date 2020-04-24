ed708a73c6059d30aeab79b4e6be34a34fbcdb73
naveenggmu <navaans@gmail.com>
2020-04-24 21:28:29 +0530

    latest

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ app=Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    to = enterShopName(request)
-    return render_template('index.html', t=to)
+    return render_template('index.html')    
+@app.route('/addShop', methods=['GET','POST'])
+def goto():
+    if(request.method=='GET'):
+        return render_template('addShop.html')
+    else:    
+        to = enterShopName(request)
+        return render_template('addShop.html', t=to)
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
