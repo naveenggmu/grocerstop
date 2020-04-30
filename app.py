e4d9493542c6f9828f699ce713bc0653db070fd9
KevinThelly <kevin.thelly@gmail.com>
2020-04-30 17:05:33 +0530

    folder reorganistaion

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ def mainpage():
 @app.route('/customerlogin', methods=['GET','POST'])
 def customerlogin():
     if request.method=="GET":
-        return render_template("customerlogin.html")
+        return render_template("Customer/customerlogin.html")
     else:
         user = authentication(request)
         if(user==0):
             print("no user")
-            return render_template("customerlogin.html")
+            return render_template("Customer/customerlogin.html")
         else:
             print(user)
             return render_template('index.html') 
@@ -26,11 +26,11 @@ def customerlogin():
 @app.route('/customerregister', methods=['GET','POST'])
 def customerregister():
     if request.method=="GET":
-        return render_template("customerregister.html")
+        return render_template("Customer/customerregister.html")
     else:
         register(request)
         print("registered")
-        return redirect(url_for('customerlogin'))
+        return redirect(url_for('Customer/customerlogin'))
 
 @app.route('/test', methods=['GET','POST'])
 def index():
@@ -44,38 +44,38 @@ def goto():
         for key,values in shops.items():
             print(key,values)
 
-        return render_template('addShop.html',t=shops)
+        return render_template('Shopkeeper/addShop.html',t=shops)
 
     else:    
         to = enterShopDetails(request)
-        return render_template('addShop.html', t=to)
+        return render_template('Shopkeeper/addShop.html', t=to)
 
 @app.route('/customerPage', methods=['GET','POST'])
 def goto1():
     if(request.method=='GET'):
         shops=db.child("shops").get().val()
         print(shops)
-        return render_template('customerPage.html',t=shops)
+        return render_template('Customer/customerPage.html',t=shops)
     else:
         shops=locationwise(request)
         print(shops)
-        return render_template('customerPage.html',t=shops)        
+        return render_template('Customer/customerPage.html',t=shops)        
 
 @app.route('/addcustomer', methods=['GET','POST'])
 def addcustomers():
     if(request.method=='GET'):
-        return render_template('addcustomer.html')
+        return render_template('Shopkeeper/addcustomer.html')
     else:
         countchange(request)
-        return render_template('addcustomer.html')
+        return render_template('Shopkeeper/addcustomer.html')
 
 @app.route('/addProducts', methods=['GET','POST'])
 def addProd():
     if(request.method=='GET'):
-        return render_template('addProducts.html')
+        return render_template('Shopkeeper/addProducts.html')
     else:
         data = addProducts(request)
-        return render_template('addProducts.html', data = data)            
+        return render_template('Shopkeeper/addProducts.html', data = data)            
 
 @app.route('/customerBookingShop/<shopid>',methods=['GET','POST'])
 def custBook(shopid):
@@ -86,7 +86,7 @@ def custBook(shopid):
                 'shop_products' : x,
                 'shopid': shopid,
                 'bookingStatus': ""}
-        return render_template('customerBookingShop.html',data = data)
+        return render_template('Customer/customerBookingShop.html',data = data)
     elif(request.method=='POST'):
         print("Into elif")
         bookingstatus(request)
@@ -95,7 +95,7 @@ def custBook(shopid):
                 'shop_products' : x,
                 'shopid': shopid,
                 'bookingStatus': "Updated"}
-        return render_template('customerBookingShop.html',data = data)    
+        return render_template('Customer/customerBookingShop.html',data = data)    
 
 
 
